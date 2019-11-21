66cec4bdae330749421ac79da040b932a45afd41
elly <41842121+ellyzinha@users.noreply.github.com>
2019-11-21 19:02:52 -0300

    nada

--- main_another_world.c
+++ main_another_world.c
@@ -1,12 +1,5 @@
 /*******************************************************************************************
-*
-*   raylib [textures] example - Background scrolling
-*
-*   This example has been created using raylib 2.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2019 Ramon Santamaria (@raysan5)
-*
+
 ********************************************************************************************/
 
 #include "raylib.h"
