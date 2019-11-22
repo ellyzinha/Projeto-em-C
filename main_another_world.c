6191f3094f6dcdba780683e6be06426398fcd537
elly <41842121+ellyzinha@users.noreply.github.com>
2019-11-21 21:27:30 -0300

    implementação do sprite animado

--- main_another_world.c
+++ main_another_world.c
@@ -1,120 +1,76 @@
-/*******************************************************************************************
-
-********************************************************************************************/
-
 #include "raylib.h"
+
+const int screenWidth = 1200;
+const int screenHeight = 800;
 
 typedef struct {
     Vector2 pos;
-    Vector2 vel;
-    Vector2 acc;
-    int largura;
-    int altura;
-}PLAYER;
+    Texture2D textura;
+    Rectangle sourceRec;
+    int frameAtual;
+}PERSONAGEM;
 
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1200;
-    const int screenHeight = 800;
+void initPersonagem(PERSONAGEM* personagem){
+    personagem->pos.x = 0;
+    personagem->pos.y = 650;
+    personagem->textura = LoadTexture("./pink_monster/monstrograndeconceito.png");
+    Rectangle sr = {0, 0, personagem->textura.width/6, personagem->textura.height};
+    personagem->sourceRec = sr;
+    personagem->frameAtual = 0;
+}
 
-    InitWindow(screenWidth, screenHeight, "Another World");
-    
-    PLAYER jogador;
-    
-    jogador.altura = 30;
-    jogador.largura = 20;
-    jogador.pos.x = 0;
-    jogador.pos.y = jogador.altura;
-    jogador.vel.x = 0;
-    jogador.vel.y = 0;
-    jogador.acc.x = 0;
-    jogador.acc.y = 0.2;
-
-    // NOTE: Be careful, background width must be equal or bigger than screen width
-    // if not, texture should be draw more than two times for scrolling effect
-    Texture2D background = LoadTexture("C:/Users/elly/Pictures/forest.png");
-    Texture2D sprite = LoadTexture("C:/Users/elly/Pictures/scarfy2.png");
-
-
-
-    float scrollingBack = 0.0f;
-
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-void DrawPlayer(PLAYER* jogador) {
-        DrawRectangle(jogador->pos.x, jogador->pos.y, jogador->largura, jogador->altura, LIGHTGRAY);
-    }
-    
-    void UpdatePlayer(PLAYER* jogador) {
+void updatePersonagem(PERSONAGEM* personagem) {
+    static int frameCounter = 0;
+    frameCounter++;
+    if(frameCounter > 10) {
+        frameCounter = 0;
+        personagem->frameAtual++;
         
-        if(IsKeyDown(KEY_D)) jogador->pos.x += 7;
-        if(IsKeyDown(KEY_A)) jogador->pos.x -= 7;
-       
-        if(IsKeyPressed(KEY_SPACE) && jogador->pos.y == screenHeight - jogador->altura) {
-            jogador->vel.y = -8;
+        if(personagem->frameAtual > 5) {
+            personagem->frameAtual = 0;
         }
-        
-        
-        jogador->vel.x += jogador->acc.x;
-        jogador->vel.y += jogador->acc.y;
-        
-        jogador->pos.x += jogador->vel.x;
-        jogador->pos.y += jogador->vel.y;
-        
-        if(jogador->pos.x < 0) jogador->pos.x = 0;
-        if(jogador->pos.x + jogador->largura > screenWidth) jogador->pos.x = screenWidth - jogador->largura;
-        if(jogador->pos.y < 0) jogador->pos.y = 0;
-        if(jogador->pos.y + jogador->altura > screenHeight) jogador->pos.y = screenHeight - jogador->altura;
     }
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
+    
+    personagem->sourceRec.x = personagem->frameAtual*personagem->textura.width/6;
+}
+
+void drawPersonagem(PERSONAGEM* personagem) {
+    DrawTextureRec(personagem->textura, personagem->sourceRec, personagem->pos, WHITE);
+}
+
+int main(void){
+    
+    InitWindow(screenWidth, screenHeight, "Monitoria");
+    SetTargetFPS(60);
+    
+    //Cenário
+    Texture2D background = LoadTexture("C:/Users/elly/Pictures/forest.png");
+    
+    float scrollingBack = 0.0f;
+    
+    SetTargetFPS(60);
+    
+    PERSONAGEM personagem;
+    initPersonagem(&personagem);
+    
+    while(!WindowShouldClose()) {
+        
         scrollingBack -= 0.3f;
-       
-
-        // NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
+         
         if (scrollingBack <= -background.width*2) scrollingBack = 0;
-      
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+         
+        DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+        DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            // Draw background image twice
-            // NOTE: Texture is scaled twice its size
-            DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
-            UpdatePlayer(&jogador);
-            DrawPlayer(&jogador);
-            
-            //Sprite Texture
-            DrawTexture(sprite, 300, 300, WHITE);
-
-       
-            
-            
-           
-
+        ClearBackground(RAYWHITE);
+        
+        updatePersonagem(&personagem);
+        drawPersonagem(&personagem);
+        
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(background);  // Unload background texture
-    UnloadTexture(sprite);
-   
-
-    CloseWindow();              // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+    
+    UnloadTexture(background);
+    
+    CloseWindow();
 }
