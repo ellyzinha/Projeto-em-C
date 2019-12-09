584f9816621aa5ff0fc378b5c433eddfb01269bc
ellyzinha <adriellylovato@gmail.com>
2019-12-09 20:57:53 -0300

    ajuste da animação dos sprites, animação do sprite parado e pulando

--- Valley/Code.c
+++ Valley/Code.c
@@ -3,12 +3,17 @@
 const int screenWidth = 1200;
 const int screenHeight = 800;
 
+
 typedef struct {
     Vector2 pos;
     Vector2 vel;
     Vector2 acc;
     Texture2D textura;
+    Texture2D textura_idle;
+    Texture2D textura_jump;
     Rectangle sourceRec;
+    Rectangle sourceRec_idle;
+    Rectangle sourceRec_jump;
     int frameAtual;
     int altura;
     int largura;
@@ -23,7 +28,7 @@ typedef struct {
 
 void initObstaculo(OBSTACULO* obstaculo){
     obstaculo->pos_obs.x = 600;
-    obstaculo->pos_obs.y = 400;
+    obstaculo->pos_obs.y = 600;
     obstaculo->altura_obs;
     obstaculo->largura_obs;
     obstaculo->textura_obs = LoadTexture("./obstaculos/brown_on.png");
@@ -39,13 +44,21 @@ void initPersonagem(PERSONAGEM* personagem){
     personagem->acc.x = 0;
     personagem->altura;
     personagem->largura;
+    personagem->textura_idle = LoadTexture("./pink_monster/pink_monster.png");
+    Rectangle sr_idle = {0.0f, 0.0f, personagem->textura_idle.width/4, personagem->textura_idle.height};
+    personagem->sourceRec_idle = sr_idle;
     personagem->textura = LoadTexture("./pink_monster/monstrograndeconceito.png");
-    Rectangle sr = {0, 0, personagem->textura.width/6, personagem->textura.height};
+    Rectangle sr = {0.0f, 0.0f, personagem->textura.width/6, personagem->textura.height};
     personagem->sourceRec = sr;
+    personagem->textura_jump = LoadTexture("./pink_monster/pink_monster_jump.png");
+    Rectangle sr_jump = {0.0f, 0.0f, personagem->textura_jump.width/8, personagem->textura_jump.height};
+    personagem->sourceRec_jump = sr_jump;
+    
     personagem->frameAtual = 0;
 }
 
 void updatePersonagem(PERSONAGEM* personagem) {
+   
     static int frameCounter = 0;
     frameCounter++;
     if(frameCounter > 10) {
@@ -56,17 +69,22 @@ void updatePersonagem(PERSONAGEM* personagem) {
             personagem->frameAtual = 0;
         }
     }
+
     if (IsKeyDown(KEY_RIGHT))
     {
         personagem->pos.x += 2;
+        personagem->sourceRec.x = personagem->frameAtual*personagem->textura.width/6;
+        
     }
     if (IsKeyDown(KEY_LEFT))
     {
         personagem->pos.x -= 2;
+        personagem->sourceRec.x = personagem->frameAtual*personagem->textura.width/6;
     }
-    if (IsKeyDown(KEY_UP))
+    if (IsKeyDown(KEY_UP) && (personagem->pos.y > 500) )
     {
         personagem->vel.y = -5;
+        personagem->sourceRec_jump.x = personagem->frameAtual*personagem->textura_jump.width/8;
     }
     
     personagem->vel.x += personagem->acc.x;
@@ -81,12 +99,19 @@ void updatePersonagem(PERSONAGEM* personagem) {
     if(personagem->pos.y  > 650) personagem->pos.y = 650;
     
     
-    personagem->sourceRec.x = personagem->frameAtual*personagem->textura.width/6;
+    personagem->sourceRec_idle.x = personagem->frameAtual*personagem->textura_idle.width/4;
 }
 
 //Função que desenha o personagem.
 void drawPersonagem(PERSONAGEM* personagem) {
-    DrawTextureRec(personagem->textura, personagem->sourceRec, personagem->pos, WHITE);
+    if(IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_LEFT)){
+        DrawTextureRec(personagem->textura, personagem->sourceRec, personagem->pos, WHITE);
+    }else if(IsKeyDown(KEY_UP)){
+        DrawTextureRec(personagem->textura_jump, personagem->sourceRec_jump, personagem->pos, WHITE);
+    }else{
+        DrawTextureRec(personagem->textura_idle, personagem->sourceRec_idle, personagem->pos, WHITE);
+    }
+    
 }
 
 //Função que desenha os obstaculos durante o cenário.
@@ -100,7 +125,7 @@ int main(void){
     SetTargetFPS(60);
     
     //Cenário
-    Texture2D background = LoadTexture("./background/forest.png");
+    Texture2D background = LoadTexture("./background/level1/forest.png");
     
     float scrollingBack = 0.0f;
     
